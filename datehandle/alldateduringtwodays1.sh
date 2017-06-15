4eafd962dc58adbb8f03f44cd64609f7a4613e4a
vfhky <vfhky@qq.com>
2017-06-15 17:16:52 +0800

    封装成函数，方便其它程序调用.

--- datehandle/alldateduringtwodays1.sh
+++ datehandle/alldateduringtwodays1.sh
@@ -6,22 +6,75 @@
 # https://github.com/vfhky/shell-tools/blob/master/datehandle/alldateduringtwodays1.sh
 
 
-if [[ $# != 3 ]]; then
-	echo "Usage: $0 2017-04-01 2017-06-14 -  or  $0 20170401 20170614 - ."
+if [[ $# -le 2 || $# -gt 3 ]]; then
+	echo "Usage: $0 2017-04-01 2017-06-14 [-]  or  $0 20170401 20170614 [-] ."
 	exit 1
 fi
 
-start_day=$(date -d "$1" +%s)
-end_day=$(date -d "$2" +%s)
+START_DAY=$(date -d "$1" +%s)
+END_DAY=$(date -d "$2" +%s)
 # The spliter bettwen year, month and day.
 SPLITER=${3}
 
-while (( "${start_day}" <= "${end_day}" )); do
-	day=$(date -d @${start_day} +"%Y${SPLITER}%m${SPLITER}%d")
-	echo "Handing date=[${day}]."
+
+# Declare an array to store all the date during the two days you inpute.
+declare -a DATE_ARRAY
+
+
+function genAlldate
+{
+	if [[ $# -ne 3 ]]; then
+		echo "Usage: genAlldate 2017-04-01 2017-06-14 [-]  or  genAlldate 20170401 20170614 [-] ."
+		exit 1
+	fi
 	
-	# We should use start_day other ${start_day} here.
-	start_day=$((${start_day}+86400))
+	START_DAY_TMP=${1}
+	END_DAY_TMP=${2}
+	SPLITER_TMP=${3}
+	I_DATE_ARRAY_INDX=0
 	
-	#sleep 1
+	# while [[ "${START_DAY}" -le "${END_DAY}" ]]; do
+	while (( "${START_DAY_TMP}" <= "${END_DAY_TMP}" )); do
+		cur_day=$(date -d @${START_DAY_TMP} +"%Y${SPLITER_TMP}%m${SPLITER_TMP}%d")
+		DATE_ARRAY[${I_DATE_ARRAY_INDX}]=${cur_day}
+		
+		# We should use START_DAY_TMP other ${START_DAY_TMP} here.
+		START_DAY_TMP=$((${START_DAY_TMP}+86400))
+		((I_DATE_ARRAY_INDX++))
+		
+		#sleep 1
+	done
+}
+
+# Call the funciotn to generate date during the two days you inpute.
+genAlldate "${START_DAY}" "${END_DAY}" "${SPLITER}"
+
+
+# [Method 1] Traverse the array.
+echo -e "[Method 1] Traverse the array."
+for SINGLE_DAY in ${DATE_ARRAY[@]};
+do
+	echo ${SINGLE_DAY}
 done
+
+
+# [Method 2] Traverse the array.
+echo -e "\n[Method 2] Traverse the array."
+for i in "${!DATE_ARRAY[@]}"; do 
+    printf "%s\t%s\n" "$i" "${DATE_ARRAY[$i]}"
+done
+
+
+# [Method 3] Traverse the array.
+echo -e "\n[Method 3] Traverse the array."
+i=0
+while [ $i -lt ${#DATE_ARRAY[@]} ]
+do
+    echo ${DATE_ARRAY[$i]}
+    let i++
+done
+
+# If you do not need this array any more, you can unset it.
+# unset DATE_ARRAY
+
+exit 0
