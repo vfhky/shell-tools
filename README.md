3e065802d6855464056e6484317139dbaf51fa82
vfhky <vfhky@qq.com>
2017-10-07 22:43:30 +0800

    增加同步发布pelican文章到coding.net的脚本。

--- README.md
+++ README.md
@@ -10,24 +10,33 @@
 
 ### 2.1 backup目录
 
-1. 2015-06-29	mysql_backup.sh：[《2015博客升级记(七)：CentOS7定时备份MySql的shell脚本》](https://typecodes.com/linux/centos7mysqlregularbackup.html '查看原文')
-2. 2015-12-18	nginx_log_backup.sh：[《Linux下Nginx、MySQL和PHP等应用的日志切割脚本》](https://typecodes.com/linux/applogsbackup.html '查看原文')
-3. 2015-06-30	typecodes_backup.sh：[《2015博客升级记(八)：CentOS7定时备份博客程序的shell脚本》](https://typecodes.com/linux/centos7blogregularbackup.html '查看原文')
+| 日期        | 脚本文件           | 说明文档  |
+| --------   | -----:   | :----: |
+| 2015-12-18      | nginx_log_backup.sh      |   [《Linux下Nginx、MySQL和PHP等应用的日志切割脚本》](https://typecodes.com/linux/applogsbackup.html '查看原文') |
+| 2015-06-30 | typecodes_backup.sh      |    [《2015博客升级记(八)：CentOS7定时备份博客程序的shell脚本》](https://typecodes.com/linux/centos7blogregularbackup.html '查看原文') |
+| 2015-06-29      | mysql_backup.sh | [《2015博客升级记(七)：CentOS7定时备份MySql的shell脚本》](https://typecodes.com/linux/centos7mysqlregularbackup.html '查看原文') |
 
 
 ### 2.2 synchronize目录
 
-1. 2016-01-10	github_pelican_nginx.sh：[《同步GitHub Markdown文章到Linux服务器后更新Pelican博客的脚本》](https://typecodes.com/linux/syngithubmarkdownpelican.html '查看原文')
+| 日期        | 脚本文件           | 说明文档  |
+| --------   | -----:   | :----: |
+| 2017-10-07      | codingnet_pelican_nginx.sh | 同步Coding.net Markdown文章到Linux服务器后更新Pelican博客的脚本 |
+| 2016-01-10      | github_pelican_nginx.sh | [《同步GitHub Markdown文章到Linux服务器后更新Pelican博客的脚本》](https://typecodes.com/linux/syngithubmarkdownpelican.html '查看原文') |
 
 
 ### 2.3 filehandle目录
 
-1. 2016-06-18	handle_makefile.sh：[《使用sed命令批量处理Makefile文件的脚本》](https://typecodes.com/linux/handlemakefilebysed.html '查看原文')
-2. 2017-02-22	sedawkfindreplace1.sh：[《使用sed和awk查找和替换字符串处理Makefile文件（一）》](https://typecodes.com/linux/sedawkfindreplace1.html '查看原文')
-3. 2017.3.9	sedawkfindreplace2.sh：[《使用sed和awk查找和替换字符串处理Makefile文件（二）》](https://typecodes.com/linux/sedawkfindreplace2.html '查看原文')
-4. 2017.5.22	sedawkfindreplace3.sh：[《使用sed和awk查找和替换字符串处理Makefile文件（三）》](https://typecodes.com/linux/sedawkfindreplace3.html '使用sed和awk查找和替换字符串处理Makefile文件（三）')
+| 日期        | 脚本文件           | 说明文档  |
+| --------   | -----:   | :----: |
+| 2017-05-22 | sedawkfindreplace3.sh      |    [《使用sed和awk查找和替换字符串处理Makefile文件（三）》](https://typecodes.com/linux/sedawkfindreplace3.html '使用sed和awk查找和替换字符串处理Makefile文件（三）') |
+| 2017-03-09 | sedawkfindreplace2.sh      |    [《使用sed和awk查找和替换字符串处理Makefile文件（二）》](https://typecodes.com/linux/sedawkfindreplace2.html '查看原文') |
+| 2017-02-22      | sedawkfindreplace1.sh      |   [《使用sed和awk查找和替换字符串处理Makefile文件（一）》](https://typecodes.com/linux/sedawkfindreplace1.html '查看原文') |
+| 2016-06-18      | handle_makefile.sh | [《使用sed命令批量处理Makefile文件的脚本》](https://typecodes.com/linux/handlemakefilebysed.html '查看原文') |
 
 
 ### 2.4 datehandle目录
 
-1. 2017.6.15	alldateduringtwodays1.sh：[《shell遍历输出两个日期范围内所有的日期》](https://typecodes.com/linux/alldateduringtwodays1.html '查看原文')
+| 日期        | 脚本文件           | 说明文档  |
+| --------   | -----:   | :----: |
+| 2017-06-15      | alldateduringtwodays1.sh | [《shell遍历输出两个日期范围内所有的日期》](https://typecodes.com/linux/alldateduringtwodays1.html '查看原文') |
---
+++ synchronize/codingnet_pelican_nginx.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+# FileName:		 codingnet_pelican_nginx.sh
+# Description:	 Synchronize markdown articles with github, convert to html files using Pelican, deliver it to nginx environment.
+# Simple Usage:	 ./codingnet_pelican_nginx.sh "commit_comments"
+# Crontab Usage: 00 01 * * * /mydata/backups/bak_list/codingnet_pelican_nginx.sh >/dev/null 2>&1
+# (c) 2016 vfhky https://typecodes.com/linux/syngithubmarkdownpelican.html
+# https://CodingNet/vfhky/shell-tools/blob/master/synchronize/codingnet_pelican_nginx.sh
+
+
+# Basic command.
+FINDCMD="find"
+MVCMD="\\mv -f"
+CPCMD="\\cp -rf"
+RMCMD="\\rm -rf"
+TARXCMD="tar -zxf"
+TARZIPCMD="tar --warning=no-file-changed -zcf"
+
+# Pelican compile markdown files to html.
+PELICAN_COMPILE_DIR=/mydata/GitBang/pelican
+# Private bang in github for store your markdown files.
+CODINGNET_PELICAN_DIR=/mydata/GitBang/CodingNet/BlogBak
+# Backup dir for your website's version management.
+PELICAN_TAR_DIR=/usr/share/nginx/html/pelican_content_bak
+# Dir of your website in nginx server.
+PELICAN_BLOG_DIR=/usr/share/nginx/html/pelican
+# Dir for this shell script to generate logs automatically.
+BLOG_PUBLISH_LOG_DIR=/mydata/backups/logs/blogpublish
+# Your personal homepage in github.
+GITHUB_PERSONAL_PAGE=/mydata/GitBang/GitHub/vfhky.github.io
+# Articles in 15 minutes are legal.
+TIME_GAP=55
+
+# Get the newest file name.
+#Newest_File="ls -lrt| tail -n 1 | awk '{print $9}'"
+
+
+# Current date format: e.g 20150505_2015.
+Current_Date=$(date +%Y%m%d_%H%M)
+
+# Check if current user is root.
+[ "$(id -u)" != "0" ] && echo "Error: You must be root to run this script." && exit 1
+
+# Check parameter.
+if [ $# -gt 1 ]; then
+	echo "Usage:	./codingnet_pelican_nginx.sh 1 \"commit_comments\"" && exit 1
+fi
+
+# Run command functions.
+function ERROR() {
+	echo >/dev/null && echo "[$(date +%H:%M:%S:%N)][error] $*" >> "${BLOG_PUBLISH_LOG_DIR}"/"${Current_Date}".log
+	exit 1
+}
+
+function NOTICE() {
+	echo >/dev/null && echo "[$(date +%H:%M:%S:%N)][notice] $*" >> "${BLOG_PUBLISH_LOG_DIR}"/"${Current_Date}".log
+}
+
+function RUNCMD() {
+	echo "[$(date +%H:%M:%S:%N)][notice] $*" >> "${BLOG_PUBLISH_LOG_DIR}"/"${Current_Date}".log
+	eval "$@"
+}
+
+# Git pull command function.
+function Git_Pull(){
+  	RUNCMD "git pull origin master >/dev/null"
+}
+
+# Git commit command function.
+function Git_Commit(){
+	if [ $# -ne 1 ]; then
+		ERROR "Usage: Git_Commit commit_comments!"
+		exit 1;
+	else
+		RUNCMD "git pull && git add --all && git commit -m \"$2\" && git push origin master"
+	fi
+}
+
+# Get the path of markdown articles in TIME_GAP minutes.
+# function Get_Files_Path(){
+#  	 RUNCMD "${FINDCMD} . -mmin -${TIME_GAP} -type f -name \"*.md\" -print0"
+# }
+
+# Lock down permissions.You should be careful when it comes to your website for the permission of files, but it's safe using 022.
+# umask 022
+
+# Create the log dir.
+if [ ! -d $BLOG_PUBLISH_LOG_DIR ]; then
+	mkdir -p $BLOG_PUBLISH_LOG_DIR
+fi
+
+
+# Main process begin.
+NOTICE "[1]Start pull from CodingNet."
+RC=0
+RUNCMD "cd ${CODINGNET_PELICAN_DIR}/md_article && Git_Pull"
+
+RC=$?
+if [ $RC -gt 0 ]; then
+	ERROR "Git pull failed!"
+fi
+
+
+#### Method 1:Chang the IFS to del with the blank word in the filename.
+#old_IFS=$IFS
+#IFS=$(echo -en "\n\b")
+
+NOTICE "[2.1]Start copy the pulled articles to the compile dir of PELICAN."
+# New_Article_Files=$(Get_Files_Path ${CODINGNET_PELICAN_DIR}/md_article)
+# You should not delete the double quotation marks in case of existing a blank in the file path.
+#### Method 1: for New_Article_File in `${FINDCMD} . -mmin -${TIME_GAP} -type f -name "*.md"`
+#### Method 2: use the while recycle.
+${FINDCMD} . -mmin -${TIME_GAP} -type f -name "*.md"|while read -r New_Article_File
+do
+	if [ -z "${New_Article_File}" ] && [ "$1"x != "1"x ]; then
+		echo "No articles, nothing to do."
+		ERROR "No articles, nothing to do."
+	fi
+	FILE_PATH=$(dirname ${PELICAN_COMPILE_DIR}/content/articles/"${New_Article_File:2}")
+	RUNCMD "mkdir -p ${FILE_PATH} && ${CPCMD} \"${New_Article_File}\" ${FILE_PATH}"
+done
+
+#### Method 1:Recovery the IFS setting.
+#IFS=${old_IFS}
+
+RC=$?
+if [ $RC -gt 0 ]; then
+	ERROR "Copy the pulled articles failed!"
+fi
+
+NOTICE "[2.2]Start copy the pulled program to the compile dir of PELICAN."
+# New_Article_Files=$(Get_Files_Path ${CODINGNET_PELICAN_DIR}/program/pelican/pelican-vfhky)
+# You should not delete the double quotation marks in case of existing a blank in the file path.
+#### Method 1: for New_Article_File in `${FINDCMD} . -mmin -${TIME_GAP} -type f -name "*.md"`
+#### Method 2: use the while recycle.
+RUNCMD "cd ${CODINGNET_PELICAN_DIR}/program/pelican/"
+${FINDCMD} . -mmin -${TIME_GAP} -type f|while read -r New_Program_File
+do
+	if [ -z "${New_Program_File}" ]; then
+		echo "No new program file."
+		break
+	fi
+	PROGRAM_PATH=$(dirname ${PELICAN_COMPILE_DIR}/"${New_Program_File:2}")
+	RUNCMD "${CPCMD} \"${New_Program_File}\" ${PROGRAM_PATH}"
+done
+
+#### Method 1:Recovery the IFS setting.
+#IFS=${old_IFS}
+
+RC=$?
+if [ $RC -gt 0 ]; then
+	ERROR "Copy the pulled program failed!"
+fi
+
+
+NOTICE "[3]Start compile in pelican."
+RUNCMD "cd ${PELICAN_COMPILE_DIR} && make publish > /dev/null"
+
+RC=$?
+if [ $RC -gt 0 ]; then
+	ERROR "Compile in pelican failed!"
+fi
+
+
+NOTICE "[4]Start generate a tar packgage and move it to the backup dir."
+# The command of tar cause the problem that file changed as we read with the value 1, so we should ignore it using OR logic.
+RUNCMD "cd ${PELICAN_COMPILE_DIR}/output && ${TARZIPCMD} ${Current_Date}.tar.gz . || ${MVCMD} ${Current_Date}.tar.gz ${PELICAN_BLOG_DIR}"
+
+RC=$?
+if [ $RC -gt 0 ]; then
+	ERROR "Generate a tar packgage failed!"
+fi
+
+
+NOTICE "[5]Start unpack the target files."
+RUNCMD "cd ${PELICAN_BLOG_DIR} && ${TARXCMD} ${Current_Date}.tar.gz && ${MVCMD} ${Current_Date}.tar.gz ${PELICAN_TAR_DIR}"
+
+RC=$?
+if [ $RC -gt 0 ]; then
+	ERROR "Unpack the target files failed!"
+fi
+
+# if [ $# -eq 1 ]; then
+if [ -n "$2" ]; then
+	echo "Ready to synchronize to the homepage on github.com."
+	NOTICE "[6]Start copy the packgage to the local homepage bang cloned from remote in GitHub."
+	RUNCMD "${CPCMD} ${PELICAN_TAR_DIR}/${Current_Date}.tar.gz ${GITHUB_PERSONAL_PAGE} && cd ${GITHUB_PERSONAL_PAGE} && ${TARXCMD} ${Current_Date}.tar.gz && ${RMCMD} ${Current_Date}.tar.gz"
+	
+	RC=$?
+	if [ $RC -gt 0 ]; then
+		ERROR "Copy the packgage to the local homepage bang failed!"
+	fi
+	
+	NOTICE "[7]Start synchronize website to my homepage on GitHub."
+	# read -p "Please input your comments on this commitment: " COMMIT_COMMENTS
+	# while [[ -z "${COMMIT_COMMENTS}" ]]
+	# do
+	# 	read -p "Comments can not be empty.Please input again: " COMMIT_COMMENTS
+	# done
+	# RUNCMD "Git_Commit \"${COMMIT_COMMENTS}\""
+	RUNCMD "Git_Commit \"$2\""
+	
+	RC=$?
+	if [ $RC -gt 0 ]; then
+		ERROR "Synchronize website to GitHub failed!"
+	fi
+else
+	echo "Not synchronize your weibsite to the homepage on github.com."
+	NOTICE "[6]Not synchronize your weibsite to the homepage on github.com."
+fi
+
+
+NOTICE "------END------"
+exit 0
